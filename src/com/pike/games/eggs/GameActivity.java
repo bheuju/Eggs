58b0ddbd0398ba4cc6d0df3982524facdc28a90b
Bishal Heuju <b.heuju@yahoo.com>
2015-11-09 22:37:34 +0545

    Displayed texture image

--- src/com/pike/games/eggs/GameActivity.java
+++ src/com/pike/games/eggs/GameActivity.java
@@ -6,20 +6,29 @@ import org.andengine.audio.music.Music;
 import org.andengine.audio.music.MusicFactory;
 import org.andengine.audio.sound.Sound;
 import org.andengine.audio.sound.SoundFactory;
+import org.andengine.engine.Engine;
+import org.andengine.engine.FixedStepEngine;
 import org.andengine.engine.camera.Camera;
 import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.engine.options.resolutionpolicy.RelativeResolutionPolicy;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.ui.activity.BaseGameActivity;
 
 import android.util.Log;
 
+// Main Activity for the game
+
 public class GameActivity extends BaseGameActivity {
 
-	private final int CAMERA_WIDTH = 800;
-	private final int CAMERA_HEIGHT = 1280;
+	private final int CAMERA_WIDTH = 400;
+	private final int CAMERA_HEIGHT = 640;
 
 	private Camera mCamera;
 	private Scene mScene;
@@ -27,17 +36,25 @@ public class GameActivity extends BaseGameActivity {
 	Music mMusic;
 	Sound mSound;
 
+	// =========== CREATE ENGINE ===========
+	@Override
+	public Engine onCreateEngine(EngineOptions pEngineOptions) {
+		return new FixedStepEngine(pEngineOptions, 50);
+	}
+
+	// =========== CREATE ENGINE OPTIONS ===========
 	@Override
 	public EngineOptions onCreateEngineOptions() {
 
-		// defining camera object
+		// define camera object
 		mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
 
-		// declare and define engine options to be applied to engine object
+		// define engine options to be applied to engine object
 		EngineOptions engineOptions = new EngineOptions(true,
 				ScreenOrientation.PORTRAIT_SENSOR, new RatioResolutionPolicy(
 						CAMERA_WIDTH, CAMERA_HEIGHT), mCamera);
 
+		// enable sounds and music
 		engineOptions.getAudioOptions().setNeedsMusic(true);
 		engineOptions.getAudioOptions().setNeedsSound(true);
 
@@ -45,11 +62,13 @@ public class GameActivity extends BaseGameActivity {
 
 	}
 
+	// =========== CREATE RESOURCES ===========
 	@Override
 	public void onCreateResources(
 			OnCreateResourcesCallback pOnCreateResourcesCallback)
 			throws IOException {
 
+		// ----------------- Sound -----------------
 		// set search path for SoundFactory and MusicFactory
 		SoundFactory.setAssetBasePath("sfx/");
 		MusicFactory.setAssetBasePath("sfx/");
@@ -57,7 +76,7 @@ public class GameActivity extends BaseGameActivity {
 		// load sound.mp3 into sound object
 		try {
 			mSound = SoundFactory.createSoundFromAsset(getSoundManager(), this,
-					"sound.mp3");
+					"bounce.mp3");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -70,10 +89,23 @@ public class GameActivity extends BaseGameActivity {
 			e.printStackTrace();
 		}
 
+		// ----------------- Textures -----------------
+		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/eggs/");
+
+		mBitmapTextureAtlas = new BitmapTextureAtlas(getTextureManager(), 500,
+				500);
+		mRectangle1 = BitmapTextureAtlasTextureRegionFactory.createFromAsset(
+				mBitmapTextureAtlas, this, "egg.png", 0, 0);
+		mBitmapTextureAtlas.load();
+		
 		pOnCreateResourcesCallback.onCreateResourcesFinished();
 
 	}
 
+	BitmapTextureAtlas mBitmapTextureAtlas;
+	ITextureRegion mRectangle1, mRectangle2;
+
+	// =========== CREATE SCENES ===========
 	@Override
 	public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback)
 			throws IOException {
@@ -84,10 +116,15 @@ public class GameActivity extends BaseGameActivity {
 		pOnCreateSceneCallback.onCreateSceneFinished(mScene);
 	}
 
+	// =========== POPULATE SCENE ===========
 	@Override
 	public void onPopulateScene(Scene pScene,
 			OnPopulateSceneCallback pOnPopulateSceneCallback)
 			throws IOException {
+
+		Sprite egg = new Sprite(100, 100, mRectangle1,
+				getVertexBufferObjectManager());
+		mScene.attachChild(egg);
 
 		pOnPopulateSceneCallback.onPopulateSceneFinished();
 
